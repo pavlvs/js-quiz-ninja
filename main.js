0bc265763d60f6a1c5e4800004fca1a8b20dfd9c
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-11 14:29:15 +0100

    wip

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const question = 'What is Superman\'s real name?'
-const answer = prompt(question)
+const quiz = [
+    ['What is Superman\'s real name?', 'Clark Kent'],
+    ['What is Wonder Woman\'s real name?', 'Diana Prince'],
+    ['What is Batman\'s real name?', 'Bruce Wayne']
+]
 
+let score = 0 // initialize score
 
-alert(`You answered ${answer}`)
+for (const [question, answer] of quiz) {
+    const response = prompt(question)
+    if (response === answer) {
+        alert('Correct')
+        score++
+    } else {
+        alert(`Wrong! The correct anser was ${answer}`)
+    }
+}
+
+// At the end of the game , report the player's score
+alert(`Game over,  you scored ${score} points`)
