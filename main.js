879bf71b3d60814f4bf5605259480e9cda9d2ac2
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-12 18:43:44 +0100

    wip

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const quiz = [
     { name: 'Batman', realName: 'Bruce Wayne' },
 ]
 
+// View object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target, content, attributes) {
+        for (const key in attributes) {
+            target.setAttribute(key, attributes[key])
+        }
+        target.innerHTML = content
+    }
+}
+
 const game = {
     start(quiz) {
         this.questions = [...quiz]
@@ -18,20 +32,24 @@ const game = {
     },
     ask() {
         const question = `What is ${this.question.name}'s real name?`
+        view.render(view.question, question)
         const response = prompt(question)
         this.check(response)
     },
     check(response) {
         const answer = this.question.realName
         if (response === answer) {
+            view.render(view.result, 'Correct!', { 'class': 'correct' })
             alert('Correct!')
             this.score++
+            view.render(view.score, this.score)
         } else {
+            view.render(view.result, `Wrong! The correct answer was ${answer}`, { 'class': 'wrong' })
             alert(`Wrong! The correct answer was ${answer}`)
         }
     },
     gameOver() {
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
+        view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
     }
 }
 
