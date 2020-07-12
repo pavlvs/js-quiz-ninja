92d27ce715d521b3e6fa89519568b40caed78819
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-12 23:23:25 +0100

    wip

--- main.js
+++ main.js
@@ -10,17 +10,25 @@ const view = {
     question: document.getElementById('question'),
     result: document.getElementById('result'),
     info: document.getElementById('info'),
+    start: document.getElementById('start'),
     render(target, content, attributes) {
         for (const key in attributes) {
             target.setAttribute(key, attributes[key])
         }
         target.innerHTML = content
+    },
+    show(element) {
+        element.style.display = 'block'
+    },
+    hide(element) {
+        element.style.display = 'none'
     }
 }
 
 const game = {
     start(quiz) {
         this.questions = [...quiz]
+        view.hide(view.start)
         this.score = 0
         // main game loop
         for (const question of this.questions) {
@@ -50,7 +58,10 @@ const game = {
     },
     gameOver() {
         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
+        view.show(view.start)
     }
 }
 
-game.start(quiz)
+// game.start(quiz)
+
+view.start.addEventListener('click', () => game.start(quiz), false)
